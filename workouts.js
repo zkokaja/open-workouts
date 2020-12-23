177a2a5d910e7b0ce759c7e8e912620140f657bd
Zaid Kokaja <zkokaja@gmail.com>
2020-12-22 23:15:28 -0500

    Add some style and a index page, maybe sound

---
+++ workouts.js
@@ -0,0 +1,26 @@
+const workouts = [
+        {
+            'name': 'The Scientific 7-Minute Workout',
+            'author': 'Gretchen Reynolds',
+            'description': '12 exercises deploying only body weight, a chair and a wall, it fulfills the latest mandates for high-intensity effort -- all of it based on science.',
+            'steps': [
+                { 'name': 'Get ready', 'duration_sec': 5, 'type': 'rest', },
+                { 'name': 'Jumping Jacks', 'duration_sec': 10, 'type': 'activity', },
+                { 'name': 'rest', 'duration_sec': 10, type: 'rest'},
+                // { 'name': 'Wall Sit', 'duration_sec': 30, 'type': 'activity', },
+                // { 'name': 'rest', 'duration_sec': 10, type: 'rest'},
+                { 'name': "You're done!", 'duration_sec': 0, 'color': '#f2243c', }
+            ]
+        },
+        {
+            'name': 'The Advanced 7-Minute Workout',
+            'author': 'Gretchen Reynolds',
+            'description': 'A more demanding nine-step regimen than the original 7-Minute Workout, one that requires a couple of dumbbells but still takes only seven minutes.',
+            'steps': [
+                { 'name': 'Get ready', 'duration_sec': 5, 'type': 'rest', },
+                { 'name': 'Reverse Lunges', 'duration_sec': 10, 'type': 'activity', },
+                { 'name': 'rest', 'duration_sec': 10, type: 'rest'},
+                { 'name': "You're done!", 'duration_sec': 0, 'color': '#f2243c', },
+            ]
+        }
+]
